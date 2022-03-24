f17f5f123d96d639d7a125c819333ef9d7e5e47d
Ivan Rocha <777ivanrocha@gmail.com>
2022-03-24 20:54:32 -0300

    Compra de Pizzas - parte 4

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ pizzaJson.map((item, index)=>{
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2).replace(".",",")}`;
+    pizzaItem.querySelector('a').addEventListener('click', (e)=> {
+        e.preventDefault();
+
+        c('.pizzaWindowArea').style.opacity = 0;
+        c('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(() => {
+            c('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
+        
+    });
 
     c('.pizza-area').append(pizzaItem);
 });
